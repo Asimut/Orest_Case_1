94a6f80dd98b9678651f667c1d6a4d9824adf8e4
Asimut <Asimut007@gmail.com>
2020-05-22 15:37:47 +0300

    Oleg

---
+++ story_content/user.js
@@ -0,0 +1,76 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "6bGaX5zZZLF":
+        Script1();
+        break;
+      case "5p4hJ6IdqxH":
+        Script2();
+        break;
+  }
+}
+
+function Script1()
+{
+  var head = document.getElementsByTagName('head')[0];
+
+var script = document.createElement('script');
+
+script.src = '//code.jquery.com/jquery-1.11.0.min.js';
+
+script.type = 'text/javascript';
+
+head.appendChild(script);
+}
+
+function Script2()
+{
+  var player = GetPlayer();
+
+WEB_APP_URL="https://script.google.com/macros/s/AKfycbyGlm3bo5ahumZH7ntILrkgtsS6gWg5u7uLs8QFqjh9H9yI5gE/exec";  
+
+storyline =
+
+{
+
+ "UserName" : player.GetVar("UserName"),
+
+  "Region" : player.GetVar("Region"),
+
+  "Video_pass" : player.GetVar("Video_pass")
+ }
+
+//Не вносить изменений
+
+$.ajax(
+
+ {
+
+  url: WEB_APP_URL,
+
+  type: "POST",
+
+  data: storyline,
+
+  success: function(data)
+
+  {
+
+   console.log(data);
+
+  },
+
+  error: function(err)
+
+  {
+
+   console.log('Error:', err);
+
+  }
+
+ });
+
+ return false;
+}
+
